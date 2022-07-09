5ff27ff303e83fe95e9fd328f6dbe9fea45c0d2c
yGaR <ygar@rambler.ru>
2022-07-09 15:25:45 +0300

    Task4

---
+++ Task4/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FinalTask
+{
+    [Serializable]
+    class Person
+    {
+        public Person(string name, string group, DateTime date)
+        {
+            Name = name;
+            Group = group;
+            DateOfBirth = date;
+        }
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public static Person[]? GetData(string path)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    return (Person[])bf.Deserialize(fs);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return null;
+        }
+
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿using FinalTask;
+
+Console.WriteLine(Directory.GetCurrentDirectory());
+DirectoryInfo di = new DirectoryInfo(Directory.GetCurrentDirectory());
+for (int i = 0; i < 3; i++)
+     di = di.Parent;
+Console.WriteLine(di.FullName);
+string fileName = di.FullName + "\\Students.dat";
+if (File.Exists(fileName))
+{
+    Person[] p = Person.GetData(fileName);
+    if(p != null)
+    {
+        foreach (Person p2 in p)
+        {
+            Console.WriteLine(p2.Name);
+        }
+    }
+}
+else
+{
+    Console.WriteLine("File not found");
+}
+
